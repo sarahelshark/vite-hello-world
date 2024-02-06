0d1a9f1ed70a30b176c361640be762bb37276ef2
sarahelshark <sarahelsh183@gmail.com>
2024-02-06 19:03:01 +0100

    test text interpolation
    input type with v-model

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <script >
 export default {
-  name:'App',
-  data(){
+  name: 'App',
+  data() {
     return {
-      message:'sarah',
-      content:'lorem ipsum dolor'
+      message: 'sarah',
+      content: 'lorem ipsum dolor',
+      msg:'lorem'
     }
   }
 }
 </script>
 
 <template>
- 
-    <div class="wrapper">
-      <h1>Hello {{ message }}</h1>
-      <p>{{ content }}</p>
-    </div>
-  
+  <div class="wrapper">
+    <h1>Hello {{ message }}</h1>
+    <p>{{ content }}</p>
+    <div>length:{{ msg.length }}</div>
 
-  
+    <input type="text" v-model="msg">
+  </div>
 </template>
 
-<style >
-
-</style>
+<style ></style>
